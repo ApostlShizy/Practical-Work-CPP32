88ecdc492b05e25dd869b1ecfe5880d4f9533c60
ApostlShizy <WalkLU@yandex.ru>
2025-02-19 07:13:33 +0300

    PW-32-V1.0.1

--- Task_2/main.cpp
+++ Task_2/main.cpp
@@ -1,76 +1,79 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
-#include <map>
 #include <string>
 #include "nlohmann/json.hpp"
 
 struct Film {
+    struct Actor
+    {
+        std::string name;
+        std::string role;
+        Actor(std::string inName,std::string inRole) : name(inName),role(inRole) {}
+    };
+    std::vector<Actor> actors;
+    std::string filmName;
     int yearProduction;
     std::string country;
     std::string genre;
     std::string director;
-    std::string screenwriter;
-    std::string filmCompany;
+    std::string scrinwriter;
     std::string producer;
-    std::map<std::string,std::string> actors;
+    
 };
 
-bool checkNum(const std::string & numStr) {
-    for(int i = 0;i < numStr.length();++i){
-        if(numStr[i] < '0' || numStr[i] > '9') {
-            std::cout<<"Not a number !"<<std::endl;
-            return true;
+std::string inputStr() {
+    std::string temp;
+    std::cout<<" input : ";
+    std::getline(std::cin>>std::ws,temp);
+    return temp;
+}
+
+Film addFilm() {
+    Film tempFilm;
+    std::cout<<"\nFilm name ";
+    tempFilm.filmName = inputStr();
+    std::cout<<"\nYear of production : ";
+    std::cin>> tempFilm.yearProduction;
+    std::cout<<"\nCountry ";
+    tempFilm.country = inputStr();
+    std::cout<<"\nGenre ";
+    tempFilm.genre = inputStr();
+    std::cout<<"\nDirector ";
+    tempFilm.director = inputStr();
+    std::cout<<"\nScrinwriter ";
+    tempFilm.scrinwriter = inputStr();
+    std::cout << "\nProducer ";
+    tempFilm.producer = inputStr();
+    do {
+        char choice;
+        std::cout<<"\nAdd actor (y/n) : ";
+        std::cin>>choice;
+        if(choice == 'y') {
+            std::string tempName;
+            std::string tempRole;
+            std::cout<<"\nActor name ";
+            tempName = inputStr();
+            std::cout<<"\nRole ";
+            tempRole = inputStr();
+            tempFilm.actors.emplace_back(tempName,tempRole);
         }
-    }
-    return false;
-}
-
-std::string enterStr() {
-    std::string tempName;        
-    std::getline(std::cin>>std::ws,tempName);
-    return tempName;
-}
-
-Film makeFilm() {
-    Film tempFilm;    
-    std::string tempStr;
-    do {
-        std::cout<<"\nEnter year of production : ";
-        std::cin>>tempStr;
-    } while(checkNum(tempStr));
-    tempFilm.yearProduction = std::stoi(tempStr);
-    std::cout<<"\nEnter film country : ";
-    tempFilm.country = enterStr();
-    std::cout<<"\nEnter film genre : ";
-    tempFilm.genre = enterStr();
-    std::cout<<"\nEnter film director : ";
-    tempFilm.director = enterStr();
-    std::cout<<"\nEnter film screewriter : ";
-    tempFilm.screenwriter = enterStr();
-    std::cout<<"\nEnter film company : ";
-    tempFilm.filmCompany = enterStr();
-    std::cout<<"\nEnter producer : ";
-    tempFilm.producer = enterStr();
-    do {
-        std::cout<<"\nRole name : ";
-        tempStr = enterStr();
-        std::cout<<"\nActor name : ";
-        tempFilm.actors[tempStr] = enterStr();
-        std::cout<<"\nadd actor or \"end\" ?";
-        std::cin>>tempStr;
-    } while(tempStr != "end");
+        else if (choice == 'n') {
+            break;
+        }
+    }while(true);
     return tempFilm;
 }
 
-bool findStr(std::string strWhat,std::string strIn) {    
-    for(int i = 0 ; i <= strIn.length() - strWhat.length();++i) {
-        bool found = true;
-        for(int j = 0; j < strWhat.length() && found;++j) {
-            if(strIn[i+j] != strWhat[j]){
-                found = false;
+bool findActor(std::string nameSearh,std::string name) {
+    for(int i = 0; i <= name.length()-nameSearh.length();++i) {
+        bool find = true;
+        for(int j = 0; j < nameSearh.length();++j) {
+            if(name[i+j] != nameSearh[j]) {
+                find = false;
             }
         }
-        if(found){
+        if(find) {
             return true;
         }
     }
@@ -78,67 +81,50 @@ bool findStr(std::string strWhat,std::string strIn) {
 }
 
 int main () {
-    std::string choice;
-    std::cout<<"\"add\" or \"upload\" movies ? If you are a user, you can just upload, the movies are already added : ";
-    std::cin>>choice;
-    if(choice == "add") {
-        nlohmann::json dict;
-        std::ifstream in_file("cinema.json");
-        if(in_file.is_open()) {
-            in_file >> dict;
-        }        
-        in_file.close();
-        std::map<std::string,Film> cinema;  
-        std::ofstream out_file("cinema.json");
-        while(choice != "end") {
-            std::cout<<"\"add\" movie or \"end\" additing : ";
-            std::cin>>choice;
-            if(choice == "add") {
-                std::cout<<"\nEnter film name : "<<std::endl;
-                std::string filmName = enterStr();
-                cinema.insert(std::pair<std::string,Film> (filmName,makeFilm()));
-            }
-        }        
-        std::map<std::string,Film>::iterator iter = cinema.begin();
-        if(iter != cinema.end()) {
-            for(;iter != cinema.end();++iter) {
-                dict[iter->first]["yearProduction"] = iter->second.yearProduction;
-                dict[iter->first]["country"] = iter->second.country;
-                dict[iter->first]["genre"] = iter->second.genre;
-                dict[iter->first]["director"] = iter->second.director;
-                dict[iter->first]["screenwriter"] = iter->second.screenwriter;
-                dict[iter->first]["filmCompany"] = iter->second.filmCompany;
-                dict[iter->first]["producer"] = iter->second.producer;
-                for(std::map<std::string,std::string>::iterator iterActor = iter->second.actors.begin(); iterActor != iter->second.actors.end();++iterActor) {
-                    dict[iter->first]["actors"][iterActor->first] = iterActor->second;
-                }
-            }
-        }
-        out_file << dict;
-        out_file.close();
-    }  
     std::ifstream in_file("cinema.json");
     nlohmann::json dict;
-    in_file >> dict;
-    std::string nameActor;
-    while(true) {    
-        std::cout<<"\nEnter actor name, last name or \"exit\" : ";
-        std::cin>>nameActor;
-        if(nameActor == "exit") {
-            break;
-        }
-        for(nlohmann::json::iterator iterFilm = dict.begin(); iterFilm != dict.end();++iterFilm) {         
-            for(nlohmann::json::iterator iterActor = iterFilm->begin();iterActor != iterFilm->end();++iterActor) {
-                if(iterActor.key() == "actors") {
-                    for(nlohmann::json::iterator iterName = iterActor->begin();iterName != iterActor->end();++iterName) {
-                        if(findStr(nameActor,iterName.value())){
-                            std::cout<<"Actor " << iterName.value() << " Play role : " << iterName.key() <<std::endl;
-                            std::cout<<"In film : " << iterFilm.key()<<std::endl;
-                        }
-                    }
-                }
-            }            
+    if(in_file.is_open()){
+        
+    }
+    in_file.close();    
+    std::vector<Film> cinema;
+    std::string choice;
+    while(choice != "find") {        
+        std::cout<<"\nEnter add or find : ";
+        std::cin>>choice;
+        if(choice == "add") {
+            cinema.push_back(addFilm());
         }
     }
+    if(!cinema.empty()) {
+        for(auto & current : cinema) {
+            dict[current.filmName]["yearProduction"] = current.yearProduction;
+            dict[current.filmName]["country"] = current.country;
+            dict[current.filmName]["director"] = current.director;
+            dict[current.filmName]["genre"] = current.genre;
+            dict[current.filmName]["scrinwriter"] = current.scrinwriter;
+            dict[current.filmName]["producer"] = current.producer;
+            for(auto currentActors : current.actors) {
+                dict[current.filmName]["actors"][currentActors.role] = currentActors.name;                
+            }
+        }
+        std::ofstream out_File("cinema.json");
+        out_File << dict;
+        out_File.close();
+    }
+    std::string nameSearh;
+    std::cout << "\nName of actor : ";
+    std::cin >> nameSearh;
+    std::ifstream in_file2 ("cinema.json");
+    in_file2 >> dict;
     in_file.close();
+    for (auto iterFilm = dict.begin(); iterFilm != dict.end(); ++iterFilm) {
+        auto iterFilmFild = iterFilm->find("actors");
+        for (auto& iterActors = iterFilmFild->begin(); iterActors != iterFilmFild->end(); ++iterActors) {
+            if (findActor(nameSearh, iterActors.value())) {
+                std::cout << "Actor " << iterActors.value() << " Play role " << iterActors.key() << std::endl;
+                std::cout << "In film : " << iterFilm.key() << std::endl;
+            }
+        }
+    }
 }
